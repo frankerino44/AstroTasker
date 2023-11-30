e233f49c158b3d0b26f67f21322ef137f201672e
frankerino44 <fburgarino@gmail.com>
2023-11-30 16:32:09 -0600

    log in/sign up layouts & sign up firebase functionality

--- app/src/main/java/com/example/astrotasker/MainActivity.java
+++ app/src/main/java/com/example/astrotasker/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.astrotasker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void signUpRedirect(View view) {
+        redirectToSignUp();
+    }
+
+    private void redirectToSignUp() {
+        Intent redirectToSignUp = new Intent(this, SignUp.class);
+        startActivity(redirectToSignUp);
     }
 }
--- app/src/main/java/com/example/astrotasker/SignUp.java
+++ app/src/main/java/com/example/astrotasker/SignUp.java
@@ -1,14 +1,98 @@
 package com.example.astrotasker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    EditText firstNameET;
+    EditText lastNameET;
+    EditText usernameET;
+    EditText emailET;
+    EditText passwordET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("Users");
+        reference.child("Users");
+
+        reference.child("Users");
+
+        firstNameET = findViewById(R.id.firstNameET);
+        lastNameET = findViewById(R.id.lastNameET);
+        usernameET = findViewById(R.id.usernameET);
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+    }
+
+    public void signUp(View view) {
+        String firstName = firstNameET.getText().toString();
+        String lastName = lastNameET.getText().toString();
+        String username = usernameET.getText().toString();
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, save new user in Freebase
+                            String uid = reference.push().getKey();
+                            User user = new User();
+
+                            user.setFirstName(firstName);
+                            user.setLastName(lastName);
+                            user.setUsername(username);
+
+                            Toast.makeText(SignUp.this, uid,
+                                    Toast.LENGTH_SHORT).show();
+                            reference.child(uid).setValue(user);
+                            Intent intent = new Intent(SignUp.this, MainActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Exception e = task.getException();
+                            Toast.makeText(SignUp.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.e("TAG", "Authentication failed", e);
+                        }
+                    }
+                });
+    }
+
+    public void backToLogIn(View view) {
+        redirectToLogIn();
+    }
+
+    private void redirectToLogIn() {
+        Intent redirectToLogIn = new Intent(this, MainActivity.class);
+        startActivity(redirectToLogIn);
     }
 }
