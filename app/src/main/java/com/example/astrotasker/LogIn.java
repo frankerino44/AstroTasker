744abefc7dc8229cff9cdeee2f70154bd9817ba1
frankerino44 <fburgarino@gmail.com>
2023-11-30 21:16:45 -0600

    changed MainActivity to LogIn (along with xml file), successful login now redirects to Tasks, extracted some string attributes

---
+++ app/src/main/java/com/example/astrotasker/LogIn.java
@@ -0,0 +1,85 @@
+package com.example.astrotasker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LogIn extends AppCompatActivity {
+
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+    DatabaseReference reference;
+
+    EditText emailET;
+    EditText passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference();
+
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.passwordET);
+    }
+
+    public void logIn(View view) {
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            redirectToTasks();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Exception e = task.getException();
+                            if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(LogIn.this, "Account not found.",
+                                        Toast.LENGTH_SHORT).show();
+                                Log.e("TAG", "Authentication failed", e);
+                            } else {
+                                Toast.makeText(LogIn.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                Log.e("TAG", "Authentication failed", e);
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void signUpRedirect(View view) {
+        redirectToSignUp();
+    }
+
+    private void redirectToSignUp() {
+        Intent redirectToSignUp = new Intent(this, SignUp.class);
+        startActivity(redirectToSignUp);
+    }
+
+    private void redirectToTasks() {
+        Intent redirectToTasks = new Intent(this, Tasks.class);
+        startActivity(redirectToTasks);
+    }
+}
--- app/src/main/java/com/example/astrotasker/SignUp.java
+++ app/src/main/java/com/example/astrotasker/SignUp.java
@@ -4,7 +4,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +12,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
@@ -145,7 +143,7 @@ public class SignUp extends AppCompatActivity {
     }
 
     private void redirectToLogIn() {
-        Intent redirectToLogIn = new Intent(this, MainActivity.class);
+        Intent redirectToLogIn = new Intent(this, LogIn.class);
         startActivity(redirectToLogIn);
     }
 
