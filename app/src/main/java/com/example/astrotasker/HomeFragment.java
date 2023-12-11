61dd665d70421731ed684e2f20bd06f9f5dfa387
zacharypatchen <zpatchen@gmail.com>
2023-12-11 15:36:08 -0600

    updated UI for profile and home fragment. Fixed bug in home fragment so now tasks are saved when leaving and returning to fragment.

--- app/src/main/java/com/example/astrotasker/HomeFragment.java
+++ app/src/main/java/com/example/astrotasker/HomeFragment.java
@@ -1,4 +1,5 @@
 package com.example.astrotasker;// HomeFragment.java
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -24,8 +25,14 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
+// ... (Your imports)
 
 public class HomeFragment extends Fragment {
 
@@ -79,6 +86,7 @@ public class HomeFragment extends Fragment {
         spinnerCategories = view.findViewById(R.id.spinnerCategories);
         btnAddTask = view.findViewById(R.id.btnAddTask);
         radioGroupTasks = view.findViewById(R.id.radioGroupTasks);
+        //radioGroupTasks.s
         radioGroupCompletedTasks = view.findViewById(R.id.radioGroupCompletedTasks);
         textViewToDo = view.findViewById(R.id.textViewToDo);
         homeUsernameTV = view.findViewById(R.id.homeUsernameTV);
@@ -87,10 +95,15 @@ public class HomeFragment extends Fragment {
         homeProfilePhotoIV = view.findViewById(R.id.homeProfilePhotoIV);
         homeLevelProgressBar = view.findViewById(R.id.homeLevelProgressBar);
 
+        // Load user data and tasks
         getUserInfo();
 
         // Set up the spinner with categories
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(), R.array.categories, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                requireActivity(),
+                R.array.categories,
+                R.layout.spinner_dropdown_item
+        );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerCategories.setAdapter(adapter);
 
@@ -104,21 +117,31 @@ public class HomeFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Load user data and tasks again when the fragment resumes
+        getUserInfo();
+    }
+
     private void getUserInfo() {
         reference.child("Users").child(uid).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
                     User user = dataSnapshot.getValue(User.class);
+                    // Update UI with user data
                     username = user.getUsername();
                     imageNum = user.getProfilePhoto();
                     imageID = requireContext().getResources().getIdentifier("alien_" + imageNum, "drawable", requireContext().getPackageName());
                     level = user.getLevel();
                     xp = user.getXp();
                     setFields();
+
+                    // Load tasks and completed tasks
+                    loadTasks();
                 } else {
                     // User data does not exist
-
                 }
             }
 
@@ -133,7 +156,7 @@ public class HomeFragment extends Fragment {
     private void setFields() {
         homeUsernameTV.setText(username);
         homeProfilePhotoIV.setImageResource(imageID);
-
+        editTextTask.setTextColor(Color.parseColor("#4CAF50"));
         double requiredXP = 10; // XP required for the first level
         int level = 1;
         double tempXP = xp;
@@ -163,6 +186,7 @@ public class HomeFragment extends Fragment {
             // Create and configure RadioButton
             RadioButton radioButton = new RadioButton(requireContext());
             radioButton.setText(String.format("%s (%d XP)", taskText, xp));
+            radioButton.setTextColor(Color.WHITE);
             radioButton.setOnClickListener(view -> completeTask(radioButton, xp));
 
             // Add RadioButton to RadioGroup
@@ -181,8 +205,11 @@ public class HomeFragment extends Fragment {
     private void completeTask(RadioButton radioButton, int taskXP) {
         radioGroupTasks.removeView(radioButton);
         String category = spinnerCategories.getSelectedItem().toString();
+        String taskText = radioButton.getText().toString().split(" \\(")[0];
+        removeTaskFromTasks(taskText);
         RadioButton completedTaskRadioButton = new RadioButton(requireContext());
         completedTaskRadioButton.setText(String.format("%s", radioButton.getText(), taskXP));
+        completedTaskRadioButton.setTextColor(Color.WHITE);
         radioGroupCompletedTasks.addView(completedTaskRadioButton);
 
         if (radioGroupCompletedTasks.getChildCount() > 5) {
@@ -214,7 +241,25 @@ public class HomeFragment extends Fragment {
 
         levelReference.setValue(level);
     }
+    private void removeTaskFromTasks(final String taskText) {
+        // Query to find the task with the given text in the "Tasks" subcollection
+        Query query = tasksReference.orderByChild("taskText").equalTo(taskText);
 
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot taskSnapshot : dataSnapshot.getChildren()) {
+                    taskSnapshot.getRef().removeValue(); // Remove the task
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+                Log.i("CANCELLED", "CANCELLED");
+            }
+        });
+    }
 
     private int getXPForCategory(String category) {
         // Define XP values for each category
@@ -231,4 +276,75 @@ public class HomeFragment extends Fragment {
                 return 0;
         }
     }
+    private void loadTasks() {
+        loadIncompleteTasks();
+        loadCompletedTasks();
+    }
+
+    private void loadIncompleteTasks() {
+        // Load TO DO tasks from the "Tasks" subcollection and update UI
+        tasksReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                radioGroupTasks.removeAllViews(); // Clear existing tasks
+                for (DataSnapshot taskSnapshot : dataSnapshot.getChildren()) {
+                    Task task = taskSnapshot.getValue(Task.class);
+                    if (task != null) {
+                        // Create and configure RadioButton
+                        RadioButton radioButton = new RadioButton(requireContext());
+                        radioButton.setText(String.format("%s (%d XP)", task.getTaskText(), task.getXp()));
+                        radioButton.setTextColor(Color.WHITE);
+                        radioButton.setOnClickListener(view -> completeTask(radioButton, task.getXp()));
+
+                        // Add RadioButton to RadioGroup
+                        radioGroupTasks.addView(radioButton);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+                Log.i("CANCELLED", "CANCELLED");
+            }
+        });
+    }
+
+    private void loadCompletedTasks() {
+        // Load completed tasks from the "CompletedTasks" subcollection and update UI
+        completedTasksReference.orderByChild("timestamp").limitToLast(5).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                radioGroupCompletedTasks.removeAllViews(); // Clear existing completed tasks
+                List<Task> recentCompletedTasks = new ArrayList<>();
+
+                for (DataSnapshot taskSnapshot : dataSnapshot.getChildren()) {
+                    Task completedTask = taskSnapshot.getValue(Task.class);
+                    if (completedTask != null) {
+                        recentCompletedTasks.add(completedTask);
+                    }
+                }
+
+                // Display the most recent 5 completed tasks in reverse order
+                for (int i = recentCompletedTasks.size() - 1; i >= 0; i--) {
+                    Task completedTask = recentCompletedTasks.get(i);
+                    // Create and configure RadioButton for completed task
+                    RadioButton completedTaskRadioButton = new RadioButton(requireContext());
+                    completedTaskRadioButton.setText(String.format("%s", completedTask.getTaskText(), completedTask.getXp()));
+                    completedTaskRadioButton.setTextColor(Color.WHITE);
+
+                    // Add RadioButton to RadioGroup
+                    radioGroupCompletedTasks.addView(completedTaskRadioButton);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle error
+                Log.i("CANCELLED", "CANCELLED");
+            }
+        });
+    }
+
+
 }
